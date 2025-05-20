d877c5dc651b4d49866309fb72eb458d6454fc44
Zamora <X2VZAMOR@southernco.com>
2025-05-20 09:39:53 -0400

    add stuff

--- pkg/reconciler/reconciler.go
+++ pkg/reconciler/reconciler.go
@@ -1,48 +1,48 @@
 package reconciler
 
 import (
-    "github.com/rs/zerolog/log"
-    "github.com/yourusername/ansiblegitops/pkg/ansible"
-    "github.com/yourusername/ansiblegitops/pkg/git"
-    "time"
+	"github.com/rs/zerolog/log"
+	"github.com/vinnie-kaboom/ansiblegitops/pkg/ansible"
+	"github.com/vinnie-kaboom/ansiblegitops/pkg/git"
+	"time"
 )
 
 type Reconciler struct {
-    gitClient  *git.Client
-    ansible    *ansible.Runner
-    lastCommit string
+	gitClient  *git.Client
+	ansible    *ansible.Runner
+	lastCommit string
 }
 
 func NewReconciler(gitClient *git.Client, ansibleRunner *ansible.Runner) *Reconciler {
-    return &Reconciler{
-        gitClient: gitClient,
-        ansible:   ansibleRunner,
-    }
+	return &Reconciler{
+		gitClient: gitClient,
+		ansible:   ansibleRunner,
+	}
 }
 
 func (r *Reconciler) Reconcile() error {
-    commit, changed, err := r.gitClient.Pull()
-    if err != nil {
-        return err
-    }
-    if !changed && commit == r.lastCommit {
-        log.Info().Msg("No changes in repository")
-        return nil
-    }
-    log.Info().Str("commit", commit).Msg("New commit detected, running playbooks")
-    if err := r.ansible.RunPlaybooks(); err != nil {
-        return err
-    }
-    r.lastCommit = commit
-    log.Info().Str("commit", commit).Msg("Reconciliation completed")
-    return nil
+	commit, changed, err := r.gitClient.Pull()
+	if err != nil {
+		return err
+	}
+	if !changed && commit == r.lastCommit {
+		log.Info().Msg("No changes in repository")
+		return nil
+	}
+	log.Info().Str("commit", commit).Msg("New commit detected, running playbooks")
+	if err := r.ansible.RunPlaybooks(); err != nil {
+		return err
+	}
+	r.lastCommit = commit
+	log.Info().Str("commit", commit).Msg("Reconciliation completed")
+	return nil
 }
 
 func (r *Reconciler) Run(interval time.Duration) {
-    for {
-        if err := r.Reconcile(); err != nil {
-            log.Error().Err(err).Msg("Reconciliation failed")
-        }
-        time.Sleep(interval)
-    }
+	for {
+		if err := r.Reconcile(); err != nil {
+			log.Error().Err(err).Msg("Reconciliation failed")
+		}
+		time.Sleep(interval)
+	}
 }
